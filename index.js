dd92e3bb18271d9506e1e1922939d103554d5a8a
Miguel Mejias <miki.mejias85@gmail.com>
2018-01-23 16:19:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(name)() {
+function destructivelyAppendKitten(name) {
     return kittens.push(name);
 }
